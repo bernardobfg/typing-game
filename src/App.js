74fa1fc8c79d155ee1b175ab3fbb0358fbe9e21c
Bernardo Bevilaqua <bernardo.bfg@hotmail.com>
2021-09-13 09:31:41 -0300

    Initial structure

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div className="container">
+            <div className="valid-keys">
+                <span className="matched">Bern</span>
+                <span className="remainder">ardo</span>
+            </div>
+            <div className="typed-keys">
+                jsjsjsjjsj
+            </div>
+            <div className="completed-words">
+                <ol>
+                    <li>cidade</li>
+                    <li>carro</li>
+                    <li>profissional</li>
+                </ol>
+            </div>
+        </div>
+    )
+}
+
+export default App;
